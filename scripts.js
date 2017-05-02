e8824fb14a16285ca04064c8162f92338b9f13ac
Troy Walley <walley.troy@yahoo.com>
2017-05-02 16:30:39 -0400

    finished photos, added beginning of nav bar

--- scripts.js
+++ scripts.js
@@ -7,17 +7,50 @@ function ChrisPage(){
 
 	function createPhotos(){
 		webpage.pictures.forEach(function(element){
-		var indcont=document.createElement("div")
-		indcont.classList.add("individual-container")
-		var maincont=document.getElementsByClassName("main-container")[0]
-		maincont.appendChild(indcont)
-		var piccont=document.createElement("div")
-		piccont.classList.add("pic-container")
+		var indcont=document.createElement("div");
+		indcont.classList.add("individual-container");
+		var maincont=document.getElementsByClassName("main-container")[0];
+		maincont.appendChild(indcont);
+		var piccont=document.createElement("div");
+		piccont.classList.add("pic-container");
 		piccont.style.backgroundImage="url('"+element.img+"')"
-		indcont.appendChild(piccont)
+		indcont.appendChild(piccont);
+		var datecont=document.createElement("div");
+		datecont.classList.add("datecontainer");
+		var concatdate=concatDate(element.date);
+		datecont.innerHTML=concatdate;
+		indcont.appendChild(datecont);
+		var title=document.createElement("div");
+		title.classList.add("title");
+		title.innerHTML=element.title;
+		indcont.appendChild(title);
+		var categorystring=""
+		element.categories.forEach(function(cat, index){
+			if(element.categories.length>1 && index===0){
+				categorystring+=cat+", "
+			}else{
+				categorystring+=cat
+			}
+		})
+		var hr=document.createElement("hr")
+		hr.classList.add("divider")
+		indcont.appendChild(hr)
+		var category=document.createElement("div")
+		category.classList.add("category")
+		category.innerHTML=categorystring
+		indcont.appendChild(category)
 		})
 	}
-
+	function concatDate(number){
+		var months=["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+		var monthnum=number.split("/")
+		for(var i=0;i<months.length;i++){
+			if(i===monthnum[0]-1){
+				returnstring=months[i] + " " +monthnum[1]
+				return returnstring; 
+			}
+		}
+	}
 	function getInfo(){
 		 $.ajax({
 		         url: "https://api-practice-wdi.herokuapp.com/posts",
